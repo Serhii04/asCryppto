bbc79944b49541023cda1075fee7cc6e8a07c78d
roxy <godzilaalalal@gmail.com>
2023-11-09 20:41:01 +0200

    change excamples location. Move from algorithms.py file to main.py file

--- lab_3/algorithms.py
+++ lab_3/algorithms.py
@@ -275,6 +275,22 @@ def get_four_square_roots(y: int, p: int, q: int) -> [int, ...]:
     
     return y_sqrt
 
+# def get_four_square_roots(y: int, p: int, q: int) -> [int, ...]:
+#     n = p * q
+    
+#     s1 = pow(y, (p + 1) // 4, p)
+#     s2 = pow(y, (q + 1) // 4, q)
+
+#     u = pow(p, -1, q)
+#     v = pow(q, -1, p)
+
+#     y_sqrt = []
+#     for s1p in [s1, -s1]:
+#         for s2q in [s2, -s2]:
+#             y_sqrt.append((u * p * s1p + v * q * s2q) % n)
+    
+#     return y_sqrt
+
 def Decrypt(y: int, c1: int, c2: int, p: int, q: int) -> int:
     n = p * q
 
@@ -322,83 +338,6 @@ def Verify(message: int, sign: int, user: User) -> bool:
 # def ReceiveKey() -> int:
 #     pass
 
-def encrypt_decrypt_example():
-    # M = random.randint(pow(2, 64), pow(2, 301))
-    A = User()
 
-    print("A:")
-    print(A)
-    print("")
-
-    M = 0x1c52b6d7d64cc3b91b8485c8159495ebb341c8e63b5041bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-    print(f"M  = {hex(M)}")
-
-    C, C1, C2 = Encrypt(message=M, user=A)
-    print(f"C  = {hex(C)}")
-
-    DC = Decrypt(c1=C1, c2=C2, y=C, p=A.p, q = A.q)
-    print(f"DC = {hex(DC)}")
-    print(f"M  = {hex(M)}")
-
-def verification_example():
-    A = User()
-
-    p = 0xfffffffc0000000000000000000000000000000000000000000000000000023f
-    q = 0xfffffffffffffffffffffffffffffffff000000000000000000000000000002f
-    A.set_key(p=p, q=q)
-
-    print("A:")
-    print(A)
-    print("")
-
-    M = 0x1c52b6d7d64cc3b91b8485c8159495eb2d4733611638a7b341c8e63b5041bbad4b83b2abc019
-    print(f"M  = {hex(M)}")
-    S = Sign(message=M, user=A)
-    print(f"S  = {hex(S)}")
-
-    sign_is_correct = Verify(message=M, sign=S, user=A)
-    print(f"sign_is_correct = {sign_is_correct}")
-
-# def main3():
-#     A = User()
-
-#     A.set_key_server(n=0xCD845B043FE28EA28ADA650F405B0F860FEACDFA7009B597B4DC41A9945EA025)
-    
-#     M = 0x1c52b6d7d64cc3b91b8485c8159495eb2
-#     print(f"M  = {hex(M)}")
-
-#     C, C1, C2 = Encrypt(message=M, user=A)
-#     print(f"C  = {hex(C)}")
-
-#     DC = Decrypt(c1=C1, c2=C2, y=C, p=A.p, q = A.q)
-#     print(f"DC = {C1}, {C2}, {hex(DC)}")
-#     print(f"M  = {hex(M)}")
-
-# def main4():
-#     from collections import defaultdict
-#     vals = defaultdict(int)
-#     for i in range(100):
-#         a, b = GenerateKeyPair()
-
-#         vals[a] += 1
-#         vals[b] += 1
-
-#     for key in vals:
-#         print(f"{vals[key]} n umbers of {key}")
-
-
-if __name__ == "__main__":
-    # # main1()
-    # main2()
-
-    # # m1 = 0x12345678987654321
-    # # n = 0x1234567876543234567865
-
-    # # print(hex(format_message(message=m1, n=n) >> 64))
-    # # print(hex(format_message(message=m1, n=n) >> 64))
-    # # print(hex(format_message(message=m1, n=n) >> 64))
-
-    # encrypt_decrypt_example()
-    verification_example()
 
 
--- lab_3/main.py
+++ lab_3/main.py
@@ -2,17 +2,81 @@ import random
 
 import algorithms as alg
 
-def main():
-    # print(alg.get_prime_number_blum_in_interval(start=12123, end=12345432))
+def encrypt_decrypt_example():
+    # M = random.randint(pow(2, 64), pow(2, 301))
+    A = User()
 
-    M = 2134556543
-    A = alg.User()
+    print("A:")
+    print(A)
+    print("")
 
-    C, C1, C2 = alg.Encrypt(message=M, user=A)
+    M = 0x1c52b6d7d64cc3b91b8485c8159495ebb341c8e63b5041bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+    print(f"M  = {hex(M)}")
 
-    DC = alg.Decrypt(c1=C1, c2=C2, y=C, p=A.p, q = A.q)
+    C, C1, C2 = Encrypt(message=M, user=A)
+    print(f"C  = {hex(C)}")
+
+    DC = Decrypt(c1=C1, c2=C2, y=C, p=A.p, q = A.q)
     print(f"DC = {hex(DC)}")
     print(f"M  = {hex(M)}")
 
+def verification_example():
+    A = User()
+
+    p = 0xfffffffc0000000000000000000000000000000000000000000000000000023f
+    q = 0xfffffffffffffffffffffffffffffffff000000000000000000000000000002f
+    A.set_key(p=p, q=q)
+
+    print("A:")
+    print(A)
+    print("")
+
+    M = 0x1c52b6d7d64cc3b91b8485c8159495eb2d4733611638a7b341c8e63b5041bbad4b83b2abc019
+    print(f"M  = {hex(M)}")
+    S = Sign(message=M, user=A)
+    print(f"S  = {hex(S)}")
+
+    sign_is_correct = Verify(message=M, sign=S, user=A)
+    print(f"sign_is_correct = {sign_is_correct}")
+
+# def main3():
+#     A = User()
+
+#     A.set_key_server(n=0xCD845B043FE28EA28ADA650F405B0F860FEACDFA7009B597B4DC41A9945EA025)
+    
+#     M = 0x1c52b6d7d64cc3b91b8485c8159495eb2
+#     print(f"M  = {hex(M)}")
+
+#     C, C1, C2 = Encrypt(message=M, user=A)
+#     print(f"C  = {hex(C)}")
+
+#     DC = Decrypt(c1=C1, c2=C2, y=C, p=A.p, q = A.q)
+#     print(f"DC = {C1}, {C2}, {hex(DC)}")
+#     print(f"M  = {hex(M)}")
+
+# def main4():
+#     from collections import defaultdict
+#     vals = defaultdict(int)
+#     for i in range(100):
+#         a, b = GenerateKeyPair()
+
+#         vals[a] += 1
+#         vals[b] += 1
+
+#     for key in vals:
+#         print(f"{vals[key]} n umbers of {key}")
+
+
 if __name__ == "__main__":
-    main()
+    # # main1()
+    # main2()
+
+    # # m1 = 0x12345678987654321
+    # # n = 0x1234567876543234567865
+
+    # # print(hex(format_message(message=m1, n=n) >> 64))
+    # # print(hex(format_message(message=m1, n=n) >> 64))
+    # # print(hex(format_message(message=m1, n=n) >> 64))
+
+    encrypt_decrypt_example()
+    # verification_example()
